59d9c00c98f728d1c6b0c8191f3d69f9b58eebd5
tony-starikov <tony.starikov.1992@gmail.com>
2022-06-29 12:48:40 +0300

    Add contact page handler

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on port 3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
@@ -14,7 +15,12 @@ func main() {
 	}
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:tony.starikov.1992@gmail.com\">tony.starikov.1992@gmail.com</a></p>")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome!</h1>")
 }
